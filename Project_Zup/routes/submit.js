dfb0db2f2e353082ab807b70b8e63f74428fb792
gwak <rhkrtjdrb11@naver.com>
2017-06-01 16:36:44 +0900

    구글맵 장인 업데이트....

--- Project_Zup/routes/submit.js
+++ Project_Zup/routes/submit.js
@@ -1,4 +1,25 @@
+/*
+var mysql = require("mysql")
+var fs = require("fs")
+var ejs = require("ejs")
+
+var client = mysql.createConnection({
+	host: "192.168.0.70",
+	user: "root",
+	password: "zx12zx12",
+	database: "zup"
+})
+
+client.connect();
 
 exports.list = function(req, res){
-  res.render('submit', { title : "접수하기 페이지1!!!" });
+	client.query("select * from user", function(error, results) {
+		console.log(results);
+		})
+};
+
+*/
+
+exports.list = function(req, res){
+	  res.render('submit', { title : "접수하기 페이지1!!!" });
 };
