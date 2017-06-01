d48d10e8f06fc3be74a49e0dfd01993bc48d7b55
jinsky <wlsgksmf456@naver.com>
2017-06-01 16:26:57 +0900

    order,user 건드림기모띠(진하늘)

--- Project_Zup/routes/order.js
+++ Project_Zup/routes/order.js
@@ -2,8 +2,37 @@
 /*
  * GET users listing.
  */
+var mysql = require("mysql")
+var fs = require("fs")
+var ejs = require("ejs")
+
+var client = mysql.createConnection({
+	user: "root",
+	password: "zx12zx12",
+	database: "zup"
+})
 
 exports.order = function(req, res){
-  res.render("order");
+client.query("select	bl.bottlelist_num,"+ 
+						"u.user_name,"+ 
+						"u.user_phonenum,"+ 
+						"u.user_address,"+ 
+						"e.employee_name,"+ 
+						"e.employee_phonenum,"+ 
+						"b.bottle_name,"+ 
+						"bl.bottle_amount,"+
+						"bl.return_status"+
+			" from user u, employee e, bottle_list bl, bottle b"+
+			" where bl.user_num=u.user_num"+ 
+		    " and bl.employee_num=e.employee_num"+ 
+            " and bl.bottle_num=b.bottle_num"+
+            " order by bottlelist_num desc"+
+            " limit 0, 10", function(error, results) {
+			
+			res.render('order', {
+			data: results,
+			count: client.query("select count(article_num) counts from article", function(error, countresults) {console.log(countresults);})
+			})
+		})
 };
 
--- Project_Zup/routes/user.js
+++ Project_Zup/routes/user.js
@@ -2,11 +2,27 @@
 /*
  * GET users listing.
  */
+/*
+ * GET users listing.
+ */
+var mysql = require("mysql")
+var fs = require("fs")
+var ejs = require("ejs")
+
+var client = mysql.createConnection({
+	user: "root",
+	password: "zx12zx12",
+	database: "zup"
+})
 
 exports.list = function(req, res){
   res.send("respond with a resource");
 };
 
 exports.user = function(req, res){
-	  res.render("user");
+	client.query("select * from user", function(error, results) {
+		res.render('user', {
+			data: results
+			})
+		})
 	};
